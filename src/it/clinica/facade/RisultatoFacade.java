e200306059d8aa9ddff3c26dc1949dfd36277202
Simone <simone.ferraro23@gmail.com>
2016-06-11 12:16:31 +0200

    Aggiunto RisultatoFacade

--- src/it/clinica/facade/RisultatoFacade.java
+++ src/it/clinica/facade/RisultatoFacade.java
@@ -2,13 +2,31 @@ package it.clinica.facade;
 
 import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import it.clinica.dao.PrerequisitoDao;
+import it.clinica.dao.RisultatoDao;
+import it.clinica.model.Prerequisito;
 import it.clinica.model.Risultato;
 
+@Stateless(name="risultatoFacade")
 public class RisultatoFacade {
+	
+	@PersistenceContext(unitName="unit-clinica")
+	private EntityManager em;
+	
+	public RisultatoFacade(EntityManager em) {
+		this.em = em;
+	}
 
 	public List<Risultato> findAllRisultati() {
-		// TODO Auto-generated method stub
-		return null;
+		RisultatoDao dao = new RisultatoDao(this.em);
+		this.em.getTransaction().begin();
+		List<Risultato> lista = dao.findAll();
+		this.em.getTransaction().commit();
+		return lista;
 	}
 
 }
