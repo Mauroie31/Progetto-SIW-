bf67257f682d264b9c59e4bd56551dbe03941ad0
Mauroie31 <mauro.ie31@gmail.com>
2016-06-11 12:16:56 +0200

    Aggiunto RisultatoDao

--- src/it/clinica/dao/RisultatoDao.java
+++ src/it/clinica/dao/RisultatoDao.java
@@ -1,5 +1,29 @@
 package it.clinica.dao;
 
-public class RisultatoDao {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import it.clinica.model.Risultato;
+
+public class RisultatoDao extends AbstractDao<Risultato> {
+
+	public RisultatoDao(EntityManager em) {
+		super(em);
+	}
+
+	@Override
+	public Risultato findById(long id) {
+		return this.getEntityManager().find(Risultato.class, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Risultato> findAll() {
+		Query query = this.getEntityManager().createQuery("SELECT r FROM Risultati r");
+		return query.getResultList();
+	}
+	
 
 }
